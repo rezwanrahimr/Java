80590cf07355d7f76b13e56bf78d5f939f784460
rezwanrahimr <rezwanrahim69@gmail.com>
2022-09-10 12:39:28 +0600

    using javax.swing packegs

---
+++ src/DisplayingTextUsingJavaSwing.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class DisplayingTextUsingJavaSwing {
+    public static void main(String [] args){
+        JOptionPane.showMessageDialog(null,"Welcom");
+
+        System.exit(0);
+    }
+}
