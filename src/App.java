950cd4e95290f32999480ca88070324dd502bb3d
rezwanrahimr <rezwanrahim69@gmail.com>
2022-09-10 12:10:28 +0600

    pratice

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App{
+    public static void main(String [] args){
+        int NumberOne = 1;
+        int NumberTwo = 2;
+        int total = (NumberOne + NumberTwo);
+        System.out.println(total);
+    }
+}
---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String [] args){
+        System.out.println("Welcome\nto\nJava\nprogram!");
+        System.out.println("\" Welcom Again \"");
+        System.out.println("\\ Welcom");
+    }
+}
